ab5e8a345d3a177507bd7bd7d973a07fbae21e31
Ben Woodford <ben@fenneclabs.co.uk>
2017-07-29 16:21:26 +0100

    Updated SteamVR Webkit to handle OpenVR failure

--- VRUtilityBelt/Addons/AddonManager.cs
+++ VRUtilityBelt/Addons/AddonManager.cs
@@ -64,7 +64,6 @@ namespace VRUtilityBelt.Addons
             SteamVR_WebKit.SteamVR_WebKit.PostUpdateCallback += PostUpdate;
             SteamVR_WebKit.SteamVR_WebKit.PostDrawCallback += PostDraw;
 
-            // TODO: Handle OpenVR not opening.
             SteamVR_WebKit.SteamVR_WebKit.Init(new CefSharp.CefSettings()
             {
                 CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\VRUtilityBelt\\BrowserCache",
