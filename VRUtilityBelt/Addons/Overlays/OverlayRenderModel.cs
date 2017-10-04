c10ad3b9daade983b856f208e6cf9c48a7a18215
Ben Woodford <ben@fenneclabs.co.uk>
2017-10-04 17:03:11 +0100

    More work towards working render models.

--- VRUtilityBelt/Addons/Overlays/OverlayRenderModel.cs
+++ VRUtilityBelt/Addons/Overlays/OverlayRenderModel.cs
@@ -10,6 +10,9 @@ using System.Threading.Tasks;
 using Valve.VR;
 using VRUB.Utility;
 using InternalOverlay = SteamVR_WebKit.Overlay;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace VRUB.Addons.Overlays
 {
@@ -44,6 +47,10 @@ namespace VRUB.Addons.Overlays
 
         InternalOverlay _internalOverlay;
 
+        static bool _generatedOnePixelTexture = false;
+        static Texture_t _onePixelTexture;
+        static int _glOnePixelTextureId;
+
         public void Setup(Overlay parent)
         {
             _parent = parent;
@@ -51,9 +58,40 @@ namespace VRUB.Addons.Overlays
             _internalOverlay.SetAttachment(AttachmentType.Overlay, Position, Rotation, "vrub." + _parent.DerivedKey);
             _internalOverlay.SetTextureSize(Width, Height);
 
+            if (!_generatedOnePixelTexture)
+                GenerateOnePixelTexture();
+
             UpdateModel();
             _internalOverlay.Show();
-            Logger.Info("[RENDERMODEL] Setup Render Model " + _parent.DerivedKey + ".models." + Key);
+        }
+
+        static void GenerateOnePixelTexture()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            _glOnePixelTextureId = GL.GenTexture();
+            _onePixelTexture = new Texture_t();
+            _onePixelTexture.eColorSpace = EColorSpace.Linear;
+            _onePixelTexture.eType = ETextureType.OpenGL;
+            _onePixelTexture.handle = (IntPtr)_glOnePixelTextureId;
+
+            Bitmap bmp = new Bitmap(1, 1);
+            bmp.SetPixel(0, 0, Color.Transparent);
+
+            BitmapData bmpData = bmp.LockBits(
+                new Rectangle(0, 0, 1, 1),
+                ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb
+            );
+
+            GL.BindTexture(TextureTarget.Texture2D, _glOnePixelTextureId);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, 1, 1, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmpData.Scan0);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+
+            bmp.UnlockBits(bmpData);
+
+            _generatedOnePixelTexture = true;
         }
 
         void UpdateModel()
@@ -64,14 +102,21 @@ namespace VRUB.Addons.Overlays
             col.g = 1;
             col.b = 1;
 
-            string ModelFilepath = Path.IsPathRooted(ModelFile) ? ModelFile : PathUtilities.GetTruePath(_parent.BasePath, ModelFile);
-            OpenVR.Overlay.SetOverlayFromFile(_internalOverlay.Handle, ModelFile.Replace(".obj", ".png"));
+            string ModelFilePath = Path.IsPathRooted(ModelFile) ? ModelFile : PathUtilities.GetTruePath(_parent.BasePath, ModelFile);
 
-            EVROverlayError err = OpenVR.Overlay.SetOverlayRenderModel(_internalOverlay.Handle, ModelFilepath, ref col);
+            EVROverlayError err = OpenVR.Overlay.SetOverlayRenderModel(_internalOverlay.Handle, ModelFilePath, ref col);
 
             if(err != EVROverlayError.None)
             {
                 Logger.Error("[RENDERMODEL] Failed to set overlay render model: " + err.ToString());
+            } else
+            {
+                err = OpenVR.Overlay.SetOverlayTexture(_internalOverlay.Handle, ref _onePixelTexture);
+
+                if (err != EVROverlayError.None)
+                    Logger.Error("[RENDERMODEL] Failed to set overlay render model stub texture: " + err.ToString());
+                else
+                    Logger.Info("[RENDERMODEL] Set render model " + ModelFilePath + " and stub texture for " + _parent.DerivedKey + ".models." + Key);
             }
         }
 
